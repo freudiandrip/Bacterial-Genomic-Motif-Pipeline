3b901725d9d435c0f94f597a5539f28bb56612a0
System Manager <manager@bl8vbox.unassigned-domain>
2015-11-29 19:03:43 -0500

    Updating gene locations perl scripts

--- biopy_gene_locations.py
+++ biopy_gene_locations.py
@@ -1,11 +1,11 @@
-'''
-gene_locations.py takes in one .txt file of positions resulting in a motif hit
-and a .gbk file with genome + annotations of gene characterization
-*USES: Biopython, SeqIO lib: http://biopython.org/wiki/SeqIO
-'''
+#!/usr/bin/python
+
+#gene_locations.py takes in one .txt file of positions resulting in a motif hit
+#and a .gbk file with genome + annotations of gene characterization
+#*USES: Biopython, SeqIO lib: http://biopython.org/wiki/SeqIO
 
 import os
-import sys, Bio
+import sys, Bio, getopt
 from Bio import SeqIO
 
 def gene_locations(gbk, posns, motif):
@@ -15,22 +15,28 @@ def gene_locations(gbk, posns, motif):
     genes = [] #no characterization of gene --> empty string pasted in
     start_end_site = []
     product = []
-    i = 0
     #while loop to parse through gbk data; adds genes, product,
     # start + end site per annotation in CDS.
     infile_motif = open(posns, "r")
     motif_posn = infile_motif.readline()
     #checking each motif, and if there is a hit in the gbk data.
-    while motif != "":
+    while motif_posn != "":
         #while loop to traverse through gbank data:
-        while gbank.features != []:
-            feature = gbank.features[0]
-            posn = len(motif) - int(feature.qualifiers['codon_start'][0])
-            if posn <= 200 and posn > 0:
+	i = 0
+        while i < len(gbank.features):
+            feature = gbank.features[i]
+	    if feature.type == "CDS":
+              #print feature.qualifiers['codon_start']
+              #print int(motif_posn) + len(motif) - int(feature.qualifiers['codon_start'][0])
+              idx1 = str(feature.location).find("[")
+              idx2 = str(feature.location).find(":")
+              codon = str(feature.location)[idx1 + 1:idx2]
+              posn = int(motif_posn) + len(motif) - int(codon)
+              if posn <= 200 and posn > 0:
             #grabbing gbank features from ADT storage.
-            genes.append(feature.qualifiers['gene'][0])
-            start_end_site.append(feature.location)
-            product.append(feature.qualifiers['product'][0])
+                genes.append(feature.qualifiers['gene'][0])
+                start_end_site.append(feature.location)
+                product.append(feature.qualifiers['product'][0])
             #moving on to the next entry in gbank
             i += 1
         #moving on to next motif in positions file
@@ -49,6 +55,30 @@ def gene_locations(gbk, posns, motif):
     #file writing complete
     outfile.close()
 
+
+def main(argv):
+  gbk = ''
+  positions = ''
+  motif = ''
+  try:
+      opts, args = getopt.getopt(argv,"hg:p:m:",["genbank-file=","positions-file=","motif="])
+  except getopt.GetoptError:
+      print 'biopy_gene_location.py -g <genbank-file> -p <positions-file> -m <motif>'
+      sys.exit(2)
+  for opt, arg in opts:
+     if opt == '-h':
+        print 'biopy_gene_location.py -g <genbank-file> -p <positions-file> -m <motif>'
+        sys.exit()
+     elif opt in ("-g", "--genbank-file"):
+        gbk = arg
+     elif opt in ("-p", "--positions-file"):
+        positions = arg
+     elif opt in ("-m", "--motif"):
+        motif = arg
+  gene_locations(gbk, positions, motif)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
 
 
